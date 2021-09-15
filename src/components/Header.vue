ada4373af21b9e2a9ef0f2ae6a2a044223e0facb
Haythem Bekir <65097270+haythembekir20@users.noreply.github.com>
2021-09-15 14:21:06 +0100

    v1

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fa fa-viruses"></i>
+      Covid19-Tracker
+      <p>API By <a class="text-blue-300" href="https://covidapi.com"
+      target="_blank"
+      > covidapi.com</a></p>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  
+}
+</script>
